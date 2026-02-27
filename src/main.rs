83add3fa5eac728aad262da929d8c472b8ef7342
malikhuzaifabinahmed <huzaifamalik3216@gmail.com>
2026-02-28 01:41:59 +0500

    Test server

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("hello")
+}
+
+#[post("/echo")]
+async fn echo(req_body: web::Json<serde_json::Value>) -> impl Responder {
+    HttpResponse::Ok().json(req_body.into_inner())
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Starting server at http://0.0.0.0:80");
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+            .service(echo)
+    })
+    .bind(("0.0.0.0", 80))?
+    .run()
+    .await
+}
